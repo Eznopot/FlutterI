36819c58586c6d59e77611f129fc428ce8171be4
mano.baffie <mano.baffie@epitech.eu>
2022-02-25 18:25:31 +0100

    clean search

--- deezify/lib/src/screen/music/music_list.dart
+++ deezify/lib/src/screen/music/music_list.dart
@@ -3,10 +3,12 @@ import 'package:deezify/src/model/album.dart';
 import 'package:deezify/src/navigationDrawer/navigation_drawer.dart';
 import 'package:deezify/src/screen/music/music_detail.dart';
 import 'package:deezify/src/config/colors.dart';
+import 'package:deezify/src/widget/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
-class MusicListPage extends StatefulWidget{
-  const MusicListPage({Key? key}) : super(key: key);
+class MusicListPage extends StatefulWidget {
+  MusicListPage({Key? key}) : super(key: key);
+  TextEditingController searchController = TextEditingController();
 
   @override
   State<StatefulWidget> createState() {
@@ -14,35 +16,90 @@ class MusicListPage extends StatefulWidget{
   }
 }
 
-class _MusicListPage extends State<MusicListPage>{
+class _MusicListPage extends State<MusicListPage> {
+  List<Map<String, String>> musicList = Album.musicJson;
+
+  void changedList(String text) {
+    List<Map<String, String>> tmp = [];
+    musicList = Album.musicJson;
+    if (text.length < 3) {
+      return;
+    }
+    for (Map<String, String> elem in musicList) {
+      if (elem["name"] != null && elem["name"]!.compareTo(text) == 0) {
+        tmp.insert(0, elem);
+      } else if (elem["name"] != null && elem["name"]!.compareTo(text) == 1) {
+        tmp.add(elem);
+      }
+    }
+    musicList = tmp;
+  }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: DeezifyColors.appBarBackgound,
         elevation: 0,
-        title: Text("Playlist".toUpperCase(), style: TextStyle(color: Colors.white),),
+        title: Text(
+          "Playlist".toUpperCase(),
+          style: const TextStyle(color: Colors.white),
+        ),
       ),
-      drawer: navigationDrawer(),
+      drawer: const navigationDrawer(),
       body: SafeArea(
-        child: ListView.builder(
-          itemCount: Album.musicJson.length,
-          itemBuilder: (BuildContext context, int index) {
-            Map<String, String> music = Album.musicJson.elementAt(index);
-            return ListTile(
-              title: Text("${music["name"]}",),
-              subtitle: Text("${music["artist"]}",),
-              trailing: Text("${music["duration"]}",),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MusicDetailPage(
-                    artistName: music["artist"], musicName: music["name"], albumImage: music["albumImage"] ?? DeezifyImages.origin, url: music["url"] ?? "",)),
-                );
-              }
-            );
-          },
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: size.height * 0.1, horizontal: size.width * 0.1
+          ),
+          child: Column(
+            children: <Widget>[
+              CustomTextField(
+                labelText: "Search",
+                hintText: "search music title",
+                controller: widget.searchController,
+                changed: (text) {
+                  setState(() {
+                    changedList(text);
+                  });
+                }
+              ),
+              ListView.builder(
+                itemCount: musicList.length,
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, int index) {
+                  Map<String, String> music = musicList.elementAt(index);
+                  return ListTile(
+                    title: Text(
+                      "${music["name"]}",
+                    ),
+                    subtitle: Text(
+                      "${music["artist"]}",
+                    ),
+                    trailing: Text(
+                      "${music["duration"]}",
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MusicDetailPage(
+                            artistName: music["artist"],
+                            musicName: music["name"],
+                            albumImage: music["albumImage"] ??
+                                DeezifyImages.origin,
+                            url: music["url"] ?? "",
+                          )
+                        ),
+                      );
+                    }
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
